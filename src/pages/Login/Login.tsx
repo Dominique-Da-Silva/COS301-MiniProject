dc55074f87811399863c97ba86b3ce9ccbc1a3d9
Quintin <u21513768@tuks.co.za>
2024-03-14 14:52:51 +0200

    Update Login.tsx

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,26 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { supabase } from "@config/supabase";
 import { loggedInUserStore } from '@store/index';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const {userData, loginUser} = loggedInUserStore((state) => { return { userData: state.user, loginUser: state.loginUser}; });
+  const navigate = useNavigate(); // Initialize useNavigate hook
+  const { userData, loginUser } = loggedInUserStore((state) => { return { userData: state.user, loginUser: state.loginUser}; });
 
+  useEffect(() => {
+    // Create a new async function
+    const checkUser = async () => {
+      // Check if user is already logged in
+      const { data: { user } } = await supabase.auth.getUser()
+      if (user) {
+        navigate('/profile'); // Redirect to profile page if user is logged in
+      }
+    };
+  
+    // Call the async function
+    checkUser();
+  }, [navigate]);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: any) => {
     event.preventDefault();
 
-    const res = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
 
-    if (res.error) {
-      console.error('Error logging in:', res.error.message);
+    if (error) {
+      console.error('Error logging in:', error.message);
     } else {
-      console.log('User logged in:', res.data.user);
-      loginUser(res.data.user); // Update user data state
+      console.log('User logged in:', data);
+      loginUser(data.user); // Update user data state
+      navigate('/profile'); // Redirect user to /profile page
     }
   };
 
