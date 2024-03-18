022d5dde3ff2bea791c45b6a466d9e9f9a5d26d6
Loading31115 <kamogelomoeketse@gmail.com>
2024-03-19 01:01:52 +0200

    aligned tweet elements better

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -20,33 +20,34 @@ interface TweetProps {
 
 const Tweet: React.FC<TweetProps> = ({name, username, text, imageUrl, timeDisplay, likes, retweets, comments, saves,}) => {
   return (
-    <div className="tweet border">
-      <div className="user-info flex gap-3 p-3">
+    <div className="tweet flex border">
+      <div className="avatar p-3">
         <Avatar
           src={imageUrl} // profile image url to be replaced
           alt="User Avatar"
-          className="user-avatar min-w-12 min-h-12"
+          className="user-avatar min-w-12 min-h-12 p"
           // style={{ minWidth: '48px', minHeight: '48px' }}
         />
-          <div className="info-block flex-col">
-            <div className="flex">
-              <h4 className="font-semibold">{name}&nbsp;</h4>
-              <p className="text-slate-700">{username} · {timeDisplay}</p>
-            </div>
-            <p>{text}</p><br />
-              {imageUrl && (
-                <img src={imageUrl} alt="Tweet Image" className="tweet-image" style={{ borderRadius: '10px' }} />
-              )}
-        
-              {/* ICOONS */}
-            <div className="tweet-actions flex gap-10">
-              <span className="action flex"><FaRegComment /> {comments} </span> {/* Reply icon */}
-              <span className="action flex"><AiOutlineRetweet /> {retweets} </span> {/* Retweet icon */}
-              <span className="action flex"><IoMdHeartEmpty /> {likes} </span> {/* Like icon */}
-              <span className="action flex"><MdSaveAlt /> {saves} </span> {/* Share icon */}
-            </div>
-          </div>
       </div>
+      <div className="post flex-col w-full">
+        <div className="user-info flex-col gap-3 p-3">   
+          <div className="flex">
+            <h4 className="font-semibold">{name}&nbsp;</h4>
+            <p className="text-slate-700">{username} · {timeDisplay}</p>
+          </div>
+          <p>{text}</p>
+            {imageUrl && (
+              <img src={imageUrl} alt="Tweet Image" className="tweet-image" style={{ borderRadius: '10px' }} />
+            )}
+        </div>
+        <div className="tweet-actions flex flex-row justify-around">
+          <span className="action flex items-center"><FaRegComment /> &nbsp;{comments} </span> {/* Reply icon */}
+          <span className="action flex items-center"><AiOutlineRetweet /> &nbsp;{retweets} </span> {/* Retweet icon */}
+          <span className="action flex items-center"><IoMdHeartEmpty /> &nbsp;{likes} </span> {/* Like icon */}
+          <span className="action flex items-center"><MdSaveAlt /> &nbsp;{saves} </span> {/* Share icon */}
+        </div>
+      </div>
+      
     </div>
   );
 };
