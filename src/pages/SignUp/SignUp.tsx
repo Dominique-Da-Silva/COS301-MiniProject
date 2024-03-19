6ca9f993acf40a69b9aa48ba57d56b61e1f2abfd
Michael <u21546551@tuks.co.za>
2024-03-19 14:09:50 +0200

    Refactor SignUp component to use signUpNewUser utility function and useNavigate hook

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,26 +1,20 @@
 import { useState } from 'react';
-import { supabase } from "@config/supabase";
-import { loggedInUserStore } from '@store/index';
+import { signUpNewUser } from '@utils/index';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const {userData, loginUser, logoutUser} = loggedInUserStore((state) => { return { userData: state.user, loginUser: state.loginUser, logoutUser: state.logoutUser}; });
+  const [form, setForm] = useState({ email: '', password: '' });
+  const navigate = useNavigate(); // Initialize useNavigate hook
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const res = await supabase.auth.signUp({
-      email,
-      password,
-    });
+    const status = await signUpNewUser(form.email, form.password);
 
-    if (res.error) {
-      console.error('Error signing up:', res.error.message);
+    if (status === "error") {
+      console.error('Error signing up');
     } else {
-      console.log('User signed up:', res.data.user);
-      console.log(res.data.user);
-      if(res.data.user)loginUser(res.data.user); // Update user data state
+      navigate('/profile'); // Redirect to profile page if user is logged in
     }
   };
 
@@ -29,27 +23,14 @@ const SignUp = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Email:
-          <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+          <input type="email" value={form.email} onChange={e => setForm({ ...form, email: e.target.value })} required />
         </label>
         <label>
           Password:
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+          <input type="password" value={form.password} onChange={e => setForm({ ...form, password: e.target.value })} required />
         </label>
         <input type="submit" value="Sign Up" />
       </form>
-      {userData && ( // Conditionally render user data if available
-        <div>
-          <h2>User Data:</h2>
-          <p>Email: {userData.email}</p>
-          <p>UID: {userData.id}</p>
-          {/* Add more fields as needed */}
-
-          <button onClick={() => {
-            logoutUser(); // Clear user data state
-            supabase.auth.signOut();
-            }}>Sign Out</button>
-        </div>
-      )}
     </div>
   );
 };
