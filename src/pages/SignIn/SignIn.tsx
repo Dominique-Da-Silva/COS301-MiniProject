3d292d0b0f3393fa8ff728992d415fb0c29ecb79
yashie5 <u19025492@tuks.co.za>
2024-03-20 18:05:11 +0200

    feat(SignIn): initial styling for component

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -51,14 +51,15 @@ const SignIn = () => {
           <img src={github} alt="logo" className="logo mr-2 w-5 h-5"/>
           Sign in with Github
         </Button>
-        <div className="flex items-center justify-center my-2 mb-5">
+        <div className="flex items-center justify-center pb-4">
           <Divider className="w-1/3 bg-gray-300 mr-2"/>
           <span className="text-gray-500">or</span>
           <Divider className="w-1/3 bg-gray-300 ml-2"/>
         </div>
-        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 pt-2">
           <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-            <Input variant="underlined"
+            <Input 
+              variant="underlined"
               type="email"
               placeholder="Phone, email, or username"
               value={form.email}
@@ -66,7 +67,7 @@ const SignIn = () => {
               required
             />
           </div>
-          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4"> 
             <Input variant="underlined"
               type="password"
               placeholder="Password"
@@ -75,10 +76,10 @@ const SignIn = () => {
               required
             />
           </div>
-          <Button type="submit" className='w-full bg-blue-500 hover:bg-blue-600 text-white py-3 px-4 rounded-lg'>SignIn</Button>
+          <Button type="submit" className='w-full bg-blue-500 hover:bg-blue-600 text-white rounded-lg'>SignIn</Button>
         </form>
-        <div className="text-center mt-6 flex justify-center">
-            <div className="text-blue-500 mr-10 hover:underline">Forgot Password?</div>
+        <div className="text-center mt-6 flex justify-between">
+            <div className="text-blue-500 hover:underline">Forgot Password?</div>
             <div className="text-blue-500 hover:underline"><Link to="/signup">Sign Up</Link></div>
         </div>
       </Card>
