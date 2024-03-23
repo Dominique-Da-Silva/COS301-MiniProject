9c3d262539585a6eabd3a4c0fcca5e4c963ad628
Michael <u21546551@tuks.co.za>
2024-03-23 18:46:43 +0200

    Remove commented out code in Login component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -65,20 +65,6 @@ const Login = () => {
             Already have an account? <Link to="/signin" className='text-blue-500'>Log In</Link>
           </p>
       </div>
-      
-      {/*
-      <div className='signInContainer'>
-        <h2>Sign In Page</h2>
-        
-        <Link to="/signup">Sign up with phone or email</Link>
-        <p className='terms'>
-        By singing up you agree to the <a href='https://twitter.com/en/tos'>Terms of Service</a>
-        and <a href='https://twitter.com/en/privacy'>Privacy Policy</a>, including <a href='https://help.twitter.com/en/rules-and-policies/x-cookies'> Cookie Use</a>.
-        </p>
-        <div className='login'>
-          <p>Already have an account? <Link to="/signin">Sign In</Link></p>
-        </div>
-      </div>*/}
     </div>
   );
 };
