0f8aa3eba72bcfc22dd83ae7f8137071478e3348
Michael <u21546551@tuks.co.za>
2024-03-24 21:00:01 +0200

    Remove unused imports from services/index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,5 @@
 import { signInWithGithub, signInWithGoogle, signInUser, signUpNewUser, signOut, isUserLoggedIn } from "./auth/auth";
 import { uploadProfile } from "./storage/storage";
-import {fetchUsers} from "./homeServices/getUsersData"
-import {fetchTweets}  from "./homeServices/getTweets"
 
 export { signInWithGithub, signInWithGoogle, signOut, signInUser,
-    signUpNewUser, isUserLoggedIn, uploadProfile, fetchUsers, fetchTweets };
+    signUpNewUser, isUserLoggedIn, uploadProfile };
