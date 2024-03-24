562749aea3fbcce5005ce4cb328241733caae295
PineCone85 <110378695+PineCone85@users.noreply.github.com>
2024-03-24 12:00:58 +0200

    Homepage unit Test
    Added mocking fucntions for hompage unit testing.

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,6 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import HomePage from './HomePage';
 
 test('renders HomePage component without crashing', () => {
   render(<HomePage />);
 });
+
+// Mocking the funcs used for supa base
+jest.mock('@config/supabase', () => ({
+  supabase: {
+    from: jest.fn().mockReturnValue({
+      select: jest.fn().mockReturnValue({ data: [], error: null }),
+    }),
+  },
+}));
+
+// Mocking the diffrent rendering components
+//These basically are mock functions that are called instead of the actual ones when a componnet is rendered
+
+jest.mock('@components/index', () => ({
+  Tweet: jest.fn().mockReturnValue(null),
+  TrendingTopics: jest.fn().mockReturnValue(null),
+  WhoToFollow: jest.fn().mockReturnValue(null),
+  SideNavbar: jest.fn().mockReturnValue(null),
+  CreateTweet: jest.fn().mockReturnValue(null),
+}));
