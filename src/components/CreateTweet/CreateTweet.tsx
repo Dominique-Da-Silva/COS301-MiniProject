3c366c925d913ff828ece0f26e2f0c7a4df13922
yashie5 <u19025492@tuks.co.za>
2024-03-28 22:59:10 +0200

    feat(CreateTweet): change input field to textarea and get basic structure/styling

--- src/components/CreateTweet/CreateTweet.tsx
+++ src/components/CreateTweet/CreateTweet.tsx
@@ -1,22 +1,28 @@
-import {Avatar} from "@nextui-org/react";
-import {Button} from "@nextui-org/react";
-
+import {Avatar,Button, Textarea} from "@nextui-org/react";
 
 const CreateTweet = () => {
   return (
-    <div className="tweet-input m-0 items-center py-2 px-4 border-b flex">
-      <Avatar
-          // src={imageUrl} // profile image url to be replaced
-          alt="User Avatar"
-          className="user-avatar min-w-12 min-h-12"
-          // style={{ minWidth: '48px', minHeight: '48px' }}
-        />
-      <input type="text" placeholder="What is happening?!" className="flex-1 !border-none outline-none text-base !p-2 !m-0"></input>
-      <Button radius="full" className="rounded-full bg-sky-500 text-white border-none font-bold">
-        Post
-      </Button>
+    <div className="tweet-input m-0 items-center py-2 px-4 border-b flex min-h-50">
+      <div className="self-start">
+        <Avatar
+              // src={imageUrl} // profile image url to be replaced
+              alt="User Avatar"
+              className="user-avatar min-w-12 min-h-12"
+              // style={{ minWidth: '48px', minHeight: '48px' }}
+            />
+      </div>
+      {/* decide what variant is better suited later on  */}
+      <Textarea variant="underlined" placeholder="What is happening?!" className="flex-1 border-none outline-none text-base p-2 m-0"></Textarea>
+      <div className="flex content-between flex-col self-end">
+        <Button radius="full" className="rounded-full bg-sky-500 text-white border-none font-bold">
+          Tweet
+        </Button>
+      </div>
+
     </div>
   );
 };
+
+
 
 export default CreateTweet;
