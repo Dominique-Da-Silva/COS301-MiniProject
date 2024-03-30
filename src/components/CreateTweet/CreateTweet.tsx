f7ea1bfdeaee54de3cad0a407be13faeee3cff71
yashie5 <u19025492@tuks.co.za>
2024-03-30 15:12:26 +0200

    fix(CreateTweet): fix tooltip crashing

--- src/components/CreateTweet/CreateTweet.tsx
+++ src/components/CreateTweet/CreateTweet.tsx
@@ -1,33 +1,37 @@
-import { Avatar, Button, Textarea, Tooltip} from "@nextui-org/react";
-import { GalleryIcon, GIFIcon, PollsIcon, ScheduleIcon, StickersIcon } from '@assets/index';
+import { Avatar, Button, Textarea, Tooltip } from "@nextui-org/react";
+import {
+  GalleryIcon,
+  GIFIcon,
+  PollsIcon,
+  ScheduleIcon,
+  StickersIcon,
+} from "@assets/index";
 
 const CreateTweet = () => {
-
   const handleGalleryClick = (event: any) => {
     event.preventDefault();
     // Handle Gallery click
   };
-  
+
   const handleGIFClick = (event: any) => {
     event.preventDefault();
     // Handle GIF click
   };
-  
+
   const handlePollsClick = (event: any) => {
     event.preventDefault();
     // Handle Polls click
   };
-  
+
   const handleStickersClick = (event: any) => {
     event.preventDefault();
     // Handle Stickers click
   };
-  
+
   const handleScheduleClick = (event: any) => {
     event.preventDefault();
     // Handle Schedule click
   };
-  
 
   return (
     <div className="py-2 px-4">
@@ -40,7 +44,7 @@ const CreateTweet = () => {
           // style={{ minWidth: '48px', minHeight: '48px' }}
         />
         {/* decide what variant is better suited later on  */}
-        <Textarea 
+        <Textarea
           variant="underlined"
           placeholder="What is happening?!"
           className="p-2"
@@ -49,27 +53,137 @@ const CreateTweet = () => {
       </div>
       <div className="flex justify-between items-center mt-2 mx-12">
         <div className="flex">
-          <Tooltip content="Media" placement="bottom">
+          <Tooltip
+            content="Media"
+            placement="bottom"
+            delay={500}
+            motionProps={{
+              variants: {
+                exit: {
+                  opacity: 0,
+                  transition: {
+                    duration: 0.1,
+                    ease: "easeIn",
+                  },
+                },
+                enter: {
+                  opacity: 1,
+                  transition: {
+                    duration: 0.15,
+                    ease: "easeOut",
+                  },
+                },
+              },
+            }}
+          >
             <Button isIconOnly onClick={handleGalleryClick} variant="light">
               <img src={GalleryIcon} alt="Gallery" className="w-6 h-6" />
             </Button>
           </Tooltip>
-          <Tooltip content="GIF" placement="bottom">
+          <Tooltip
+            content="GIF"
+            placement="bottom"
+            delay={500}
+            motionProps={{
+              variants: {
+                exit: {
+                  opacity: 0,
+                  transition: {
+                    duration: 0.1,
+                    ease: "easeIn",
+                  },
+                },
+                enter: {
+                  opacity: 1,
+                  transition: {
+                    duration: 0.15,
+                    ease: "easeOut",
+                  },
+                },
+              },
+            }}
+          >
             <Button isIconOnly onClick={handleGIFClick} variant="light">
               <img src={GIFIcon} alt="GIF" className="w-6 h-5" />
             </Button>
           </Tooltip>
-          <Tooltip content="Poll" placement="bottom">
+          <Tooltip
+            content="Poll"
+            placement="bottom"
+            delay={500}
+            motionProps={{
+              variants: {
+                exit: {
+                  opacity: 0,
+                  transition: {
+                    duration: 0.1,
+                    ease: "easeIn",
+                  },
+                },
+                enter: {
+                  opacity: 1,
+                  transition: {
+                    duration: 0.15,
+                    ease: "easeOut",
+                  },
+                },
+              },
+            }}
+          >
             <Button isIconOnly onClick={handlePollsClick} variant="light">
               <img src={PollsIcon} alt="Polls" className="w-5 h-6" />
             </Button>
           </Tooltip>
-          <Tooltip content="Emoji" placement="bottom">
+          <Tooltip
+            content="Emoji"
+            placement="bottom"
+            delay={500}
+            motionProps={{
+              variants: {
+                exit: {
+                  opacity: 0,
+                  transition: {
+                    duration: 0.1,
+                    ease: "easeIn",
+                  },
+                },
+                enter: {
+                  opacity: 1,
+                  transition: {
+                    duration: 0.15,
+                    ease: "easeOut",
+                  },
+                },
+              },
+            }}
+          >
             <Button isIconOnly onClick={handleStickersClick} variant="light">
               <img src={StickersIcon} alt="Emoji" className="w-5 h-5" />
             </Button>
           </Tooltip>
-          <Tooltip content="Schedule" placement="bottom">
+          <Tooltip
+            content="Schedule"
+            placement="bottom"
+            delay={500}
+            motionProps={{
+              variants: {
+                exit: {
+                  opacity: 0,
+                  transition: {
+                    duration: 0.1,
+                    ease: "easeIn",
+                  },
+                },
+                enter: {
+                  opacity: 1,
+                  transition: {
+                    duration: 0.15,
+                    ease: "easeOut",
+                  },
+                },
+              },
+            }}
+          >
             <Button isIconOnly onClick={handleScheduleClick} variant="light">
               <img src={ScheduleIcon} alt="Schedule" className="w-5 h-5" />
             </Button>
