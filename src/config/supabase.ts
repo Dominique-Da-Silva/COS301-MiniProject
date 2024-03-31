06b45e7cc53dbe7277818971e870eeee7bd0cb1c
ArisNeimanRose <129956034+ArisNeimanRose@users.noreply.github.com>
2024-03-31 20:44:11 +0200

    Update supabase.ts
    Accidently commited infomation that should not be public regarding the database

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -2,4 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 import { Database } from "../types/database.types";
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient<Database>("https://przfwtfinnjmlhcdbqbr.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByemZ3dGZpbm5qbWxoY2RicWJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5MTUzMzEsImV4cCI6MjAyNTQ5MTMzMX0.MhlRU6owGd2XWdqTSzjfRgQoKn9-w_bAJtNIkyMv2zs")
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
