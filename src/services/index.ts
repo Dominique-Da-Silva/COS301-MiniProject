3734f4b5043a8c209c6a391101aca26fc63ab2d5
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-03-31 11:21:42 +0200

    Service get user based on username
    Service function that is able to fetch user that is not logged in (not user authentication).
    Simply for use of viewing other users profiles.

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,8 @@ import { uploadProfile } from "./storage/storage";
 import { updateUsername } from './profileServices/updateUsername';
 import {fetchUsers} from "./homeServices/getUsersData"
 import {fetchTweets}  from "./homeServices/getTweets"
+import {fetchUserByUsername}  from "./usersProfileServices/getUserByUsername.ts"
 
 export { signInWithGithub, signInWithGoogle, signOut, signInUser,
     signUpNewUser, isUserLoggedIn, uploadProfile, updateUsername,
-    fetchUsers, fetchTweets};
+    fetchUsers, fetchTweets, fetchUserByUsername};
