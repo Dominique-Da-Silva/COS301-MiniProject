42312215314776b173e8cc045d434b3836db7611
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-03-31 19:04:56 +0200

    updated error handling

--- src/services/usersProfileServices/followUser.ts
+++ src/services/usersProfileServices/followUser.ts
@@ -6,20 +6,13 @@ export const followUser = async (loggedInUserId: number, userToFollowId: number)
         const { data: existingFollowData} = await supabase.from('Followers').select('id').eq('Following_Id', loggedInUserId).eq('Followed_Id', userToFollowId).single();
 
         if (existingFollowData) {
-            console.log(`User ${loggedInUserId} is already following user ${userToFollowId}`);
-            return; // Return early if the user is already following
+            return { error: "User is following this user." };
         }
 
-        // If not already following, insert a new entry in the Followers table
         await supabase.from('Followers').insert([{ Following_Id: loggedInUserId, Followed_Id: userToFollowId }]);
-
-
-        console.log(`User ${loggedInUserId} followed user ${userToFollowId}`);
-        // Return some response indicating success
         return { success: true };
 
     } catch (error) {
         console.error('Error following user:', error);
-        throw error;
     }
 };
--- src/services/usersProfileServices/unfollowUser.ts
+++ src/services/usersProfileServices/unfollowUser.ts
@@ -6,24 +6,19 @@ export const unfollowUser = async (loggedInUserId: number, userToUnfollowId: num
         const { data: existingFollowData} = await supabase.from('Followers').select('id').eq('Following_Id', loggedInUserId).eq('Followed_Id', userToUnfollowId).single();
 
         if (!existingFollowData) {
-            console.log(`User ${loggedInUserId} is not following user ${userToUnfollowId}`);
-            return; // Return early if the user is not following
+            return { error: "User is not following this user." };
         }
 
         // If following, delete the entry from the Followers table
         const { error: deleteError } = await supabase.from('Followers').delete().eq('Following_Id', loggedInUserId).eq('Followed_Id', userToUnfollowId);
 
         if (deleteError) {
-            throw deleteError;
+            return { error: "Error unfollowing user." };
         }
-
-        console.log(`User ${loggedInUserId} unfollowed user ${userToUnfollowId}`);
-        // Return some response indicating success
         return { success: true };
 
 
     } catch (error) {
         console.error('Error unfollowing user:', error);
-        throw error;
     }
 };
