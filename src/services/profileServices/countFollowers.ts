867f3a57e384c7d1775f1d5488c8c8a6fcf08b4f
Dominique-Da-Silva <u21629944@tuks.co.za>
2024-04-02 09:41:23 +0200

    Profile Page Service Functions
    Added Profile Page Service Functions to current branch.

---
+++ src/services/profileServices/countFollowers.ts
@@ -0,0 +1,18 @@
+import { supabase } from "@config/supabase";
+
+export const countFollowers = async (userId: string): Promise<number> => {
+    try {
+      const { data: followersData, error: followersError } = await supabase.from('Followers')
+      .select('Followed_Id')
+      .eq('Following_Id', userId);
+  
+      if (followersError) {
+        throw followersError;
+      }
+  
+      return followersData.length;
+
+    } catch (error) {
+      console.error('Error counting followers:', error);
+    }
+};
