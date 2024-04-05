c27ef03eff41b105ef09cef71c3db3e8f009b449
GivenChauke <chaukemnashe123@gmail.com>
2024-04-05 06:50:57 +0200

    Add basic getTrending and attach tags to getTweets

--- src/services/homeServices/getTweets.ts
+++ src/services/homeServices/getTweets.ts
@@ -84,3 +84,31 @@ const fetchTweets = async () => {
 };
 
 export { addTweet };
+
+const getTrendingTopics = async () => {
+  try {
+    const { data, error } = await supabase.functions.invoke('getTrendingTopics', {
+      body: {},
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (error) {
+      throw error;
+    }
+    return data;
+
+  } catch (error) {
+    if (error instanceof FunctionsHttpError) {
+      const errorMessage = await error.context.json()
+      console.log('Function returned an error', errorMessage)
+    } else if (error instanceof FunctionsRelayError) {
+      console.log('Relay error:', error.message)
+    } else if (error instanceof FunctionsFetchError) {
+      console.log('Fetch error:', error.message)
+    }
+    throw error; // Re-throw the error to handle it in the calling code if needed
+  }
+};
+
+ export {getTrendingTopics};
--- src/services/index.ts
+++ src/services/index.ts
@@ -8,8 +8,9 @@ import {followUser}  from "./usersProfileServices/followUser.ts"
 import {unfollowUser}  from "./usersProfileServices/unfollowUser.ts"
 import { insertProfileDetails, updateProfileDetails } from "./profileServices/updateProfileDetails";
 import { addTweet } from "./homeServices/getTweets";
+import { getTrendingTopics } from "./homeServices/getTweets.ts";
 
 export { signInWithGithub, signInWithGoogle, signOut, signInUser,
     signUpNewUser, isUserLoggedIn, uploadProfile, updateUsername,
     fetchUsers, fetchTweets, fetchUserByUsername, addTweet,
-    followUser, unfollowUser, insertProfileDetails, updateProfileDetails, addUserToDatabase};
+    followUser, unfollowUser, insertProfileDetails, updateProfileDetails, addUserToDatabase,getTrendingTopics};
