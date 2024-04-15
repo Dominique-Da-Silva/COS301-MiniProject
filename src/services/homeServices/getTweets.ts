fe15721422cea01dacb2192440e3cc22e21e099b
GivenChauke <chaukemnashe123@gmail.com>
2024-04-15 05:55:13 +0200

    add trending topics filtering

--- src/services/homeServices/getTweets.ts
+++ src/services/homeServices/getTweets.ts
@@ -118,6 +118,22 @@ const getTrendingTopics = async () => {
     throw error; // Re-throw the error to handle it in the calling code if needed
   }
 };
+/**Here's the structure of getTrending JSON data:
+
+The JSON data is an array of objects.
+Each object represents a tag and contains the following key-value pairs:
+ "Tag_Id": The ID of the tag.
+ "Tag_Name": The name of the tag.
+ "Tweet_Tags": An array of objects representing tweet tags associated with this tag. Each tweet tag object contains:
+    "Tweets": An object representing one tweet associated with this tweet tag. The tweet object contains:
+          "Content": The content of the tweet.
+          "Img_Url": The URL of the image attached to the tweet (if any).
+          "User_Id": The ID of the user who posted the tweet.
+          "Tweet_Id": The ID of the tweet.
+          "Created_at": The timestamp when the tweet was created.
+  "created_at": The timestamp when the tweet tag was created.
+"tweet_count": The count of tweets associated with this tag. */
+
 async function addTags(tweetData: any) {
   const regex = /#[^\s#]+/g; // Matches hashtags (#) followed by non-whitespace characters
   const matches = tweetData[0].Content.match(regex);
