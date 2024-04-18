4cab60fd724db041c9a65b29d5adfb9744404dd5
Loading31115 <kamogelomoeketse@gmail.com>
2024-04-18 09:58:05 +0200

    trending tags show on frontend

--- src/components/TrendingList/TrendingList.tsx
+++ src/components/TrendingList/TrendingList.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { mockTrending } from '../../mockData/mockData';
 import { MdOutlineMoreHoriz } from "react-icons/md";
-// import { useEffect } from 'react';
-// import { getTrendingTopics } from '@services/index';
+import { getTrendingTopics } from '@services/index';
 
 
 interface Topic {
@@ -29,8 +28,20 @@ const formatCount = (count: number): string | number => {
 
     
 const TrendingList: React.FC<ExploreProps> = () => {
-  const [topics] = useState<any>(mockTrending);
-//   const [Tags, setTags] = useState<any[]>([]);
+  const [topics, setTopics] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchTagsData = async () => {
+      try {
+        const tagsData = await getTrendingTopics();
+        setTopics(tagsData);
+        console.log(tagsData);
+      } catch (error) {
+        console.error('Error fetching tags:', error);
+      }
+    }
+    fetchTagsData();
+  }, []);
 
 //   useEffect(() => {
 //     const fetchTopics = async () => {
@@ -47,21 +58,21 @@ const TrendingList: React.FC<ExploreProps> = () => {
 //   },[]);
 //     console.log(Tags);
 
-    return (
-        <div>
-           {topics.map((topic: Topic) => (
-            <div key={topic.Trending_Id} className="items-center justify-between p-3 hover:bg-gray-100 dark:bg-neutral-900">
-              <div className="flex justify-between items-center">
-                <h3 className="text-[16px] font-medium">#{topic.TagName}</h3>
-                <div className="h-10 w-10 flex justify-center rounded-full align-middle items-center hover:text-sky-600 hover:bg-blue-100 p-0 m-0 cursor-pointer">
-                    <MdOutlineMoreHoriz size={20}/>
-                </div>
-              </div>
-            <p className="text-[13.5px] text-gray-500 -mt-1">{formatCount(topic.NumPosts)} posts</p>
+  return (
+    <div>
+        {topics.slice(0, 5).map((topic: Topic) => (
+        <div key={topic.Tag_Id} className="items-center justify-between p-3 hover:bg-gray-100 dark:bg-neutral-900">
+          <div className="flex justify-between items-center">
+            <h3 className="text-[16px] font-medium">#{topic.Tag_Name}</h3>
+            <div className="h-10 w-10 flex justify-center rounded-full align-middle items-center hover:text-sky-600 hover:bg-blue-100 p-0 m-0 cursor-pointer">
+                <MdOutlineMoreHoriz size={20}/>
             </div>
-          ))} 
-        </div>  
-    )
+          </div>
+        <p className="text-[13.5px] text-gray-500 -mt-1">{formatCount(topic.tweet_count)} posts</p>
+        </div>
+      ))} 
+    </div>  
+  )
 };
 
 export default TrendingList;
