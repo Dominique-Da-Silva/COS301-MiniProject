521f5b31c32f99be75cc2c925a0ccee2eb07e366
Dominique Da Silva <105222552+Dominique-Da-Silva@users.noreply.github.com>
2024-04-21 13:18:53 +0200

    Update toggleSave.ts

--- src/services/homeServices/toggleSave.ts
+++ src/services/homeServices/toggleSave.ts
@@ -15,13 +15,13 @@ export async function toggleSave(tweetId: number, userId: number): Promise<"save
         }
 
         if (existingSave) {
-            const { error: unsaveError } = await supabase.from('Save').delete().eq('Save_Id', existingSave.Save_Id);
+            const { error: unsaveError } = await supabase.from('Saves').delete().eq('Save_Id', existingSave.Save_Id);
             if (unsaveError) {
                 return "error";
             }
             return "unsaved";
         } else {
-            const { error: saveError } = await supabase.from('Save').insert([{ Tweet_Id: tweetId, User_Id: userId }]);
+            const { error: saveError } = await supabase.from('Saves').insert([{ Tweet_Id: tweetId, User_Id: userId }]);
             if (saveError) {
                 return "error";
             }
@@ -30,6 +30,5 @@ export async function toggleSave(tweetId: number, userId: number): Promise<"save
 
     } catch (error: any) {
         console.error('Error toggling save for the tweet:', error.message);
-        return error;
     }
 }
