1c0f0d18596cb0b5d980e8da792711eb14c0d654
Kyle Marshall (u22570285) <u22570285@tuks.co.za>
2024-04-21 15:09:59 +0200

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -32,5 +32,5 @@ export { signInWithGithub, signInWithGoogle, signOut, signInUser,
     followUser, unfollowUser, insertProfileDetails, updateProfileDetails, addUserToDatabase,
     fetchUserData, countFollowers, countFollowing, fetchProfileDetails,
     uploadImageAndGetURL,checkIfFollowing, getLoggedUserId, getUserData,
-    getTrendingTopics,toggleLike,toggleSave,toggleRetweet, countLikes, countRetweets, countSaves, countComments,
+    getTrendingTopics,toggleLike,toggleSave,toggleRetweet, countLikes, countRetweets, countSaves, countComments, changePassword
     };
