74ca9350003c0d849af88114d8b6593d507041e5
PineCone85 <110378695+PineCone85@users.noreply.github.com>
2024-05-01 16:43:45 +0200

    Added Setting Tests

--- src/pages/Settings/Settings.test.tsx
+++ src/pages/Settings/Settings.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
-import { describe, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AccountInfo, NotificationSettings,DisplaySettings } from "@components/index";
 import Settings from './Settings';
 
 describe('Settings component', () => {
@@ -13,3 +14,46 @@ describe('Settings component', () => {
       </Router>);
   })
 })
+
+const renderSettingsContentMock = (activeTab: string) => {
+  switch (activeTab) {
+    case "account":
+      return AccountInfo;
+    case "Notifications":
+      return NotificationSettings;
+    case "Display":
+      return DisplaySettings;
+    case "ChangePassword": 
+      return null;
+    default:
+      return null;
+  }
+};
+
+test("renderSettingsContent returns correct component", () => {
+  // Define different values for activeTab
+  const tabs = ["account", "Notifications", "Display", "ChangePassword", "unknownTab"];
+
+  tabs.forEach(tab => {
+
+    const component = renderSettingsContentMock(tab);
+
+    switch (tab) {
+      case "account":
+        expect(component).toBe(AccountInfo);
+        break;
+      case "Notifications":
+        expect(component).toBe(NotificationSettings);
+        break;
+      case "Display":
+        expect(component).toBe(DisplaySettings);
+        break;
+      case "ChangePassword":
+        expect(component).toBe(null);//since no auth with email
+        break;
+      default:
+        expect(component).toBe(null);
+        break;
+    }
+  });
+});
