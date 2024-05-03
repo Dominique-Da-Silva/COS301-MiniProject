f1d5f0ef037bea4e574129f341187302f1abc58e
yashie5 <u19025492@tuks.co.za>
2024-05-03 17:27:29 +0200

    fix(TweetDetails): fix issue of some tweets not being displayed

--- src/components/TweetDetails/TweetDetails.tsx
+++ src/components/TweetDetails/TweetDetails.tsx
@@ -22,33 +22,47 @@ const TweetDetailsPage = () => {
 
   return (
     <div className="w-full flex justify-center align-middle">
-    <div className="container flex w-full justify-center dark:bg-black">
-      <div className="nav flex justify-end w-1/5 pr-5">
-        <Nav /> {/* Include the navbar component */}
-      </div>
-      <div className="main-content flex flex-col w-full md:w-3/5 m-0 p-0 border dark:border-neutral-800">
-        <div className="flex flex-col m-0 p-0 justify-center">
-          <div>
-            {tweetDetails ? ( 
-
-
-              <Tweet  tweetid={tweetDetails.Tweet_Id}  userid={tweetDetails.User_Id} name={tweetDetails.User.Name} username={tweetDetails.User.Username} text={tweetDetails.Content} imageUrl={tweetDetails.Img_Url} profileimageurl={tweetDetails.User.Profile?.[0].Img_Url} timeDisplay={tweetDetails.timeDisplay} likes={tweetDetails.likes} retweets={tweetDetails.retweets} comments={tweetDetails.comments} saves={tweetDetails.saves} bookmarked={tweetDetails.bookmarked} author={tweetDetails.User.author} ></Tweet>
-            ) : (
-              <p>Loading tweet details...</p>
-            )}
+      <div className="container flex w-full justify-center dark:bg-black">
+        <div className="nav flex justify-end w-1/5 pr-5">
+          <Nav /> {/* Include the navbar component */}
+        </div>
+        <div className="main-content flex flex-col w-full md:w-3/5 m-0 p-0 border dark:border-neutral-800">
+          <div className="flex flex-col m-0 p-0 justify-center">
+            <div>
+              {tweetDetails ? ( 
+                <Tweet
+                  tweetid={tweetDetails.Tweet_Id}
+                  userid={tweetDetails.User_Id}
+                  name={tweetDetails.User.Name}
+                  username={tweetDetails.User.Username}
+                  text={tweetDetails.Content}
+                  imageUrl={tweetDetails.Img_Url}
+                  profileimageurl={tweetDetails.User.Profile?.[0]?.Img_Url}
+                  timeDisplay={tweetDetails.timeDisplay}
+                  likes={tweetDetails.likes}
+                  retweets={tweetDetails.retweets}
+                  comments={tweetDetails.comments}
+                  saves={tweetDetails.saves}
+                  bookmarked={tweetDetails.bookmarked}
+                  author={tweetDetails.User.author}
+                />
+              ) : (
+                <p>Loading tweet details...</p>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="sidebar-right w-1/4 ml-7 mt-2 pl-1 pr-2 hidden md:block">
-        <div className="mb-3">
-          <Search />
+        <div className="sidebar-right w-1/4 ml-7 mt-2 pl-1 pr-2 hidden md:block">
+          <div className="mb-3">
+            <Search />
+          </div>
+          <TrendingTopics />
+          <WhoToFollow users={[]} />
         </div>
-        <TrendingTopics />
-        <WhoToFollow users={[]} />
       </div>
     </div>
-  </div>
   );
+  
 };
 
 export default TweetDetailsPage;
