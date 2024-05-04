67c9e10e6799d37db3b4d84f23ac3fba3a44cfc7
Kyle Marshall (u22570285) <u22570285@tuks.co.za>
2024-05-04 19:45:46 +0200

    Update GameResults.tsx
    Finished Base Game Front end flow and styling.

--- src/components/GameResults/GameResults.tsx
+++ src/components/GameResults/GameResults.tsx
@@ -9,71 +9,87 @@ interface GameResultProps {
 
 interface EmojiData {
   name: string;
-  emoji: string; 
+  emoji: string;
 }
 
 const emojiData: EmojiData[] = [
-  { name: 'Stalker', emoji: 'images/busts_in_silhouette.png'},
-  { name: 'Day One', emoji: 'images/handshake.png'},
-  { name: 'Classic Old Pal', emoji: 'images/slightly_smiling_face.png'},
-  { name: 'Bad Friend', emoji: 'images/skull.png'},
-  { name: 'Removed Acquaintance', emoji: 'images/bust_in_silhouette.png'},
-  { name: 'Stranger', emoji: 'images/question.png'},
+  { name: 'Stalker', emoji: 'images/busts_in_silhouette.png' },
+  { name: 'Day One', emoji: 'images/handshake.png' },
+  { name: 'Classic Old Pal', emoji: 'images/slightly_smiling_face.png' },
+  { name: 'Bad Friend', emoji: 'images/skull.png' },
+  { name: 'Removed Acquaintance', emoji: 'images/bust_in_silhouette.png' },
+  { name: 'Stranger', emoji: 'images/question.png' },
 ];
-
-
 
 function getSkillLevel(score: number) {
   if (score >= 100) {
-    return { level: 'Stalker', text1: 'We strongly suggest taking a break from the platform.', text2: 'In fact, we beg you: please close the app.' };
+    return { level: 'Stalker', text: 'There\'s grass that needs touching' };
   } else if (score >= 80) {
-    return { level: 'Day One', text1: 'You\'re a seasoned user!', text2: 'You know the platform like the back of your hand.' };
+    return { level: 'Day One', text: 'You give good birthday gifts.' };
   } else if (score >= 60) {
-    return { level: 'Classic Old Pal', text1: 'You\'re getting there!', text2: 'You\'re familiar with the platform, but there\'s still more to learn.' };
+    return { level: 'Classic Old Pal', text: 'A dependable side character in your friends\' lives.' };
   } else if (score >= 40) {
-    return { level: 'Bad Friend', text1: 'You know a thing or two, but there\'s room for improvement.', text2: 'Keep exploring the platform.' };
+    return { level: 'Bad Friend', text: 'Your friends told us to tell you that they\'re disappointed.' };
   } else if (score >= 20) {
-    return { level: 'Removed Acquaintance', text1: 'You\'ve got some catching up to do.', text2: 'Try to spend some more time on the platform.' };
+    return { level: 'Removed Acquaintance', text: 'We recommend that you actually get to know your friends.' };
   } else {
-    return { level: 'Stranger', text1: 'Are you sure you\'re on the right platform?', text2: 'Maybe you should try again.' };
+    return { level: 'Stranger', text: 'Do you even know any of these people... Do they know YOU?!' };
   }
 }
 
+const getColor = (percentage: number) => {
+  if (percentage >= 100) return 'green';
+  if (percentage >= 80) return 'lightgreen';
+  if (percentage >= 60) return 'yellowgreen';
+  if (percentage >= 40) return 'yellow';
+  if (percentage >= 20) return 'orange';
+  return 'red';
+};
+
 const GameResult: React.FC<GameResultProps> = ({ correctCount, totalQuestions }) => {
   const percentage = Math.round((correctCount / totalQuestions) * 100);
-  const { level, text1, text2 } = getSkillLevel(percentage);
+  const { level, text } = getSkillLevel(percentage);
   const emoji = emojiData.find(e => e.name === level);
 
   return (
-    <Card className="w-full h-screen flex flex-col items-center justify-center">
-      <h2 className="mb-6 text-3xl font-bold">
-        Twivia
-      </h2>
-      <FaTwitter style={{ fontSize: '2rem', color: '#1DA1F2', marginBottom: '1rem' }} />
-      <h3 className="text-xl mb-2">
+    <Card className="w-full h-screen flex flex-col px-8">
+      <div className='flex justify-center'>
+        <h2 className="py-6 text-3xl font-bold">
+          Twivia
+        </h2>
+      </div>
+      <div className="flex items-center justify-center">
+        <FaTwitter style={{ fontSize: '2rem', color: '#1DA1F2', marginBottom: '1rem' }} />
+      </div>
+      <h2 className="text-xl font-bold">
         Your result:
-      </h3>
-      <h1 className="text-4xl font-display tracking-widest">
+      </h2>
+      <h1 className="text-8xl font-bold mb-2" style={{ color: getColor(percentage) }}>
         {percentage}%
       </h1>
-      <h3 className="text-xl mb-2">
-        Congratulations, your skill level is:
-      </h3>
-      <h2 className="text-4xl font-display tracking-widest">
-       {level}
-      </h2>
-      {emoji && <img src={emoji.emoji} alt={emoji.name} style={{ width: '100px', height: '100px' }} />}
-      <div className="flex flex-col items-center space-y-2 mt-4">
-        <p className="text-base">
-          {text1}
-        </p>
-        <p className="text-base">{text2}</p>
+      <div className="font-bold text-xl flex items-center justify-center py-3">
+        <h2 className="mb-2">
+          Your skill level is:
+        </h2>
       </div>
-      <Link href="/twivia" className="mt-6">
-        <Button size="lg">
-          Play Again
-        </Button>
-      </Link>
+        <h2 className="text-center text-2xl font-bold tracking-widest" style={{ color: '#1DA1F2' }}>
+          {level.toUpperCase()}
+        </h2>
+      <div className="flex items-center justify-center py-4">
+        {emoji && <img src={emoji.emoji} alt={emoji.name} style={{ width: '60px', height: '60px' }} />}
+      </div>
+      <div className="flex flex-col items-center space-y-2 mt-1">
+        <p className="">
+          {text}
+        </p>
+      </div>
+      <div className='flex justify-center'>
+        <Link href="/twivia" className="mt-6">
+        <Button className={`flex items-center space-x-2 border border-gray-300 rounded-full p-2 mt-2`}>
+            Play Again
+          </Button>
+        </Link>
+      </div>
     </Card>
   );
 };
