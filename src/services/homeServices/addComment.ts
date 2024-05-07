549daadcac9028f4d8c29e59cf416df2c64aa236
GivenChauke <chaukemnashe123@gmail.com>
2024-05-07 23:28:27 +0200

    fix comment and add tweet bugs

--- src/services/homeServices/addComment.ts
+++ src/services/homeServices/addComment.ts
@@ -1,10 +1,9 @@
 import { supabase } from "@config/supabase";
-import { addTweet } from "./addTweets";
 import { CreateCommentNotification } from "./notifications";
 
 const addComment= async (userId:number,tweetId:number,content:string)=>{
 try {
-    const { data:comment, error } = await supabase
+    const { error } = await supabase
     .from('Comments')
     .insert([
     { User_Id: userId, Tweet_Id: tweetId, Content:content },
@@ -12,17 +11,20 @@ try {
     .select()
     if(error) throw error;
 
-    CreateCommentNotification(tweetId,userId);
+    // Insert tweet data into the database
+    const { error: tweetError } = await supabase
+    .from('Tweets')
+    .insert([{ User_Id: userId, Content:content, Img_Url: null, IsTweet:false },])
+    .select()
 
-    const tweetData={
-        User_Id:userId,
-        Content:content,
-        Img_Url:null,
+    if (tweetError) {
+    throw tweetError;
     }
-    const success = addTweet(tweetData);
-    if(success !== error)
-    return comment;
 
+    // Create comment notification 
+    const success = CreateCommentNotification(tweetId, userId);
+    if(success !== error)
+    return success;
     else {
         throw success
     }
