0de18069d4dc0826b24da49361087c583296bb6d
Michael <u21546551@tuks.co.za>
2024-05-13 09:12:05 +0200

    feat: Add new profile emojis to GameResults component

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -12,9 +12,18 @@ import GIFIcon from "./GIFIcon.png"
 import PollsIcon from "./PollsIcon.png"
 import ScheduleIcon from "./ScheduleIcon.png"
 import StickersIcon from "./StickersIcon.png"
+import busts_in_silhouette from 'busts_in_silhouette.png'
+import handshake from 'handshake.png'
+import slightly_smiling_face from 'slightly_smiling_face.png'
+import skull from 'skull.png'
+import bust_in_silhouette from 'bust_in_silhouette.png'
+import question from 'question.png'
 
 
 export {
-    twitter, twitterLogo, github, google, chevron, defaultavater, profile1, profile2, profile3, GalleryIcon, GIFIcon, PollsIcon, ScheduleIcon, StickersIcon
+    twitter, twitterLogo, github, google, chevron, defaultavater, 
+    profile1, profile2, profile3, GalleryIcon, GIFIcon, PollsIcon, 
+    ScheduleIcon, StickersIcon,
+    busts_in_silhouette, handshake, slightly_smiling_face, skull, bust_in_silhouette, question
 }
 
